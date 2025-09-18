b675a9a277d006d07aeedb56242e9266e636192a
osmansyed7776 <syedosman9866505382@gmail.com>
2025-09-18 19:30:51 +0530

    Create sub.java

---
+++ sub.java
@@ -0,0 +1,13 @@
+public class SubtractionExample {
+    public static void main(String[] args) {
+        int num1 = 25;
+        int num2 = 10;
+        int result = num1 - num2;
+        System.out.println("Subtraction result: " + result); // Output: Subtraction result: 15
+
+        double d1 = 50.5;
+        double d2 = 15.2;
+        double doubleResult = d1 - d2;
+        System.out.println("Double subtraction result: " + doubleResult); // Output: Double subtraction result: 35.3
+    }
+}
