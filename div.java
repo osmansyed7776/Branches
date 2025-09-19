27bd040fb3fcb9ac60ddbaee0d957cc9360e1d9d
osmansyed7776 <syedosman9866505382@gmail.com>
2025-09-19 19:24:48 +0530

    Create div.java

---
+++ div.java
@@ -0,0 +1,10 @@
+public class IntegerDivisionExample {
+    public static void main(String[] args) {
+        int dividend = 10;
+        int divisor = 3;
+
+        // Performing integer division
+        int result = dividend / divisor; 
+        System.out.println("Result of integer division (10 / 3): " + result); // Output: 3
+    }
+}
