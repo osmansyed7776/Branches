6b90e43d6e706c738eaadb1b78d61045ea11b3b2
osmansyed7776 <syedosman9866505382@gmail.com>
2025-09-19 19:21:12 +0530

    Create mul.java

---
+++ mul.java
@@ -0,0 +1,8 @@
+public class IntegerMultiplication {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        int product = num1 * num2;
+        System.out.println("The product of " + num1 + " and " + num2 + " is: " + product);
+    }
+}
