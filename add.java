f1373b3e8d6f7a86b4160c358b1faeea92a5ded8
osmansyed7776 <syedosman9866505382@gmail.com>
2025-09-19 19:10:19 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,6 +1,7 @@
 public class AddTwoNumbers {
 
     public static void main(String[] args) {
+        // Modifing in main branch
 
         // Declare and initialize two integer variables
         int number1 = 10;
